35ceb2305070e896a6ee2a920c47d4cdcf6b1caa
Salma <salmapcs2422@gmail.com>
2024-03-02 17:46:57 +0530

    first

--- quasar-project/src/router/routes.js
+++ quasar-project/src/router/routes.js
@@ -4,21 +4,28 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      
       { path: '', component: () => import('pages/candidate/HomePage.vue') },
-      {path: '',
-        component: () => import('pages/candidate/TestPage.vue'),}
       {
         path: 'auth',
         component: () => import('pages/candidate/AuthPage.vue'),
         children: [
-          { path: 'login', component: () => import('components/modules/auth/CandidateLogin.vue'), },
-          { path: 'signup', component: () => import('components/modules/auth/CandidateSignup.vue'), },
-          { path: 'ForgotPassword', component: () => import('components/modules/auth/ForgotPassword.vue'), }
+          {
+            path: 'login', component: () => import('components/modules/auth/CandidateLogin.vue'),
+          },
+          {
+            path: 'signup', component: () => import('components/modules/auth/CandidateSignup.vue'),
+          },
+          {
+            path: 'ForgotPassword', component: () => import('components/modules/auth/ForgotPassword.vue'),
+          }
         ]
       },
-      { path: 'profile', component: () => import('pages/candidate/ProfilePage.vue') },
-     
+
+      {
+        path: 'profile', component: () => import('pages/candidate/ProfilePage.vue')
+
+      },
+
 
     ]
   },
@@ -26,43 +33,47 @@ const routes = [
 
 
   {
-    path:'/recruiter',
+    path: '/recruiter',
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('pages/recruiter/HomePage.vue') },
+
       {
         path: 'auth',
         component: () => import('pages/recruiter/AuthPage.vue'),
+
         children: [
-          { path: 'login', component: () => import('components/modules/auth/RecruiterLogin.vue'), },
-          { path: 'signup', component: () => import('components/modules/auth/RecruiterSignup.vue'), },
-          { path: 'ForgotPassword', component: () => import('components/modules/auth/RecruiterForgotPassword.vue'), }
+          {
+            path: 'login', component: () => import('components/modules/auth/RecruiterLogin.vue'),
+          },
+          {
+            path: 'signup', component: () => import('components/modules/auth/RecruiterSignup.vue'),
+          },
+          {
+            path: 'ForgotPassword', component: () => import('components/modules/auth/RecruiterForgotPassword.vue'),
+          },
         ]
       }
-      ]
-      },
-      
-      { 
-        path: 'profile',
-         component: () => import('pages/recruiter/ProfilePage.vue'), 
-          children: [
-          { path: 'recruiter', component: () => import('components/modules/profile/RecruiterProfile.vue'), },
-          { path: 'education', component: () => import('components/modules/profile/CandidateProfile.vue'), },
-          { path: 'experience', component: () => import('components/modules/profile/CandidateHomePage.vue'), }
-          ]
-      },
-  
-  
-  // {
-  //   path: '/recruiter',
-  //   component: () => import('pages/recruiter/RecruiterLoginPage.vue'),
-  //   children: [
-     
-  //     { path: 'job', component: () => import('components/modules/jobs/JobDetails.vue') },
-  //     { path: 'requirements', component: () => import('components/modules/jobs/CandidateRequirements.vue') },
-  //   ]
-  // },
+    ]
+  },
 
+  {
+    path: 'profile',
+    component: () => import('pages/recruiter/ProfilePage.vue'), children: [
+      {
+        path: 'recruiter', component: () => import('components/modules/profile/RecruiterProfile.vue'),
+      },
+      {
+        path: 'education', component: () => import('components/modules/profile/CandidateProfile.vue'),
+      },
+      {
+        path: 'experience', component: () => import('components/modules/profile/CandidateHomePage.vue'),
+      }
+    ]
+  },
+
+
+  
   // Always leave this as last one,
   // but you can also remove it
   {
