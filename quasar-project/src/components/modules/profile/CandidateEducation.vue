cea00bea02ecec263a55628e63cd5f5e585ac465
Salma <salmapcs2422@gmail.com>
2024-04-13 23:16:10 +0530

    changes

--- quasar-project/src/components/modules/profile/CandidateEducation.vue
+++ quasar-project/src/components/modules/profile/CandidateEducation.vue
@@ -1,8 +1,8 @@
 <template>
-  <q-form class="flex justify-center">
-    
-    <q-card>
-      <div class="row col q-pa-sm">
+  
+  <q-form >
+   <h6>Your highest education</h6>
+      <div class="row col">
         <div class="col" style="margin-right: 20px;">
           <label for="institution">Institution</label>
           <q-input outlined v-model="institution" placeholder="e.g. University of xyz" id="institution" required />
@@ -11,47 +11,40 @@
           <label for="degree">Degree</label>
           <q-input outlined v-model="degree" placeholder="e.g. Bachelor of Science" id="degree" required />
         </div>
-      </div>
-      <div class="row col q-pa-md">
+      </div><br>
+      <div class="row col">
         <div class="col">
           <label for="field">Field of Study</label>
           <q-input outlined v-model="fieldOfStudy" placeholder="e.g. Computer Science" id="field" required full-width />
         </div>
-      </div>
-      <div class="row col q-pa-md">
+      </div><br>
+      <div class="row col">
         <div class="col" style="margin-right: 20px;">
           <label for="start-date">Start Date</label>
           <q-input type="date" color="grey-3" outlined v-model="startDate" label="Choose date" id="start-date" required full-width>
-            <template v-slot:append>
-              <q-icon name="event" color="grey" />
-            </template>
           </q-input>
         </div>
         <div class="col">
           <label for="end-date">End Date</label>
           <q-input type="date" color="grey-3" outlined v-model="endDate" label="Choose date" id="end-date" required full-width>
-            <template v-slot:append>
-              <q-icon name="event" color="grey" />
-            </template>
           </q-input>
         </div>
-      </div>
-      <div class="row col q-pa-md">
+      </div><br>
+      <div class="row col">
         <div class="col">
           <label for="grade">Grade/CGPA</label>
           <q-input outlined v-model="grade" placeholder="e.g. 3.5/4.0" id="grade" required full-width />
         </div>
-      </div>
-      <div class="row col q-pa-md">
+      </div><br>
+      <div class="row col">
         <div class="col">
           <label for="description">Description</label>
           <q-input outlined v-model="description" placeholder="e.g. Major projects, achievements" id="description" full-width />
         </div>
-      </div>
-      <div class="row col q-pa-sm">
+      </div><br>
+      <div class="row col">
         <q-btn color="primary" text-color="white" label="Next" @click="$router.replace('./experience')" style="width: 100%;" />
       </div>
-    </q-card>
   </q-form>
 </template>
 
