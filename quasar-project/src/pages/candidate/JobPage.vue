904ab9ba3afeb66bb93023be62be6a6c3834ac57
Salma <salmapcs2422@gmail.com>
2024-07-24 14:26:50 +0530

    commit

--- quasar-project/src/pages/candidate/JobPage.vue
+++ quasar-project/src/pages/candidate/JobPage.vue
@@ -1,16 +1,20 @@
-<template>
-    <q-page class="q-pa-md">
+<!-- <template>
+    <q-page class="q-pa-lg">
       <div class="row">
+  
         <div class="col-12 col-md-3">
           <h5>500+ Freshers jobs</h5>
-          <q-card class="q-mb-md">
+          <q-card class="q-mb-lg">
             <q-card-section>
               <h6>Filters</h6>
               <div class="q-mb-md">
                 <label>Sort By</label>
-                <q-radio v-model="sort" val="Relevant" label="Relevant" />
+                <div class="row">
+                  <q-radio v-model="sort" val="Relevant" label="Relevant" />
                 <q-radio v-model="sort" val="Salary - High to low" label="Salary - High to low" />
                 <q-radio v-model="sort" val="Date posted - New to Old" label="Date posted - New to Old" />
+                </div>
+                
               </div>
               <div class="q-mb-md">
                 <label>Experience</label>
@@ -45,19 +49,7 @@
             </q-card-section>
           </q-card>
         </div>
-        <div class="col-12 col-md-3">
-          <img src="/side-image.jpg" alt="Create Profile" class="q-mb-md">
-          <q-card class="q-mb-md">
-            <q-card-section>
-              <div>Login with Apna and experience more!</div>
-              <ul>
-                <li>Personalised job matches</li>
-                <li>Direct connect with HRs</li>
-                <li>Latest updates on the job</li>
-              </ul>
-            </q-card-section>
-          </q-card>
-        </div>
+       
       </div>
     </q-page>
   </template>
@@ -102,18 +94,22 @@
   
   <style scoped>
   .req {
-    padding-top: 3px;
+    padding-top: 5px;
     height: 25px;
     width: 120px;
-    border-radius: 10px;
+    border-radius: 30px;
     text-align: center;
-    font-size: 12px;
+    font-size: 13px;
+    
   }
   .q-pa-md {
+    padding-top: 3px;
     padding: 1rem;
   }
   .q-mb-md {
     margin-bottom: 1rem;
   }
   </style>
-  
+   -->
+
+   
